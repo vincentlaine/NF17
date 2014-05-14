2bfdeea33532c46318183e3a9452da98f9a7b468
vincentlaine <vincent.laine.utc@gmail.com>
2014-05-14 22:11:37 +0200

    Create head.php

---
+++ head.php
@@ -0,0 +1,29 @@
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="../../docs-assets/ico/favicon.png">
+
+    <title>Clinique vétérinaire</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+	<link href="bootstrap/dist/css/sous-menu.css" rel="stylesheet">
+	
+	<!-- Bootstrap theme -->
+
+    <!-- Custom styles for this template -->
+	<link href="bootstrap/docs/examples/theme/theme.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="../../assets/js/html5shiv.js"></script>
+      <script src="../../assets/js/respond.min.js"></script>
+    <![endif]-->
+
+	<script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+	<script src="https://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+    <script src="bootstrap/dist/js/bootstrap.min.js"></script>
+  </head>
