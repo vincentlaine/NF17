029efcc1d135f34ab0f58f3d69de7918be0959a0
vincentlaine <vincent.laine.utc@gmail.com>
2014-05-14 22:07:49 +0200

    Create menu_veto.php

---
+++ menu_veto.php
@@ -0,0 +1,58 @@
+<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+  <div class="container">
+	<div class="navbar-header">
+	  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+	  </button>
+	  <a class="navbar-brand" href="#">Clinique vétérinaire</a>
+	</div>
+	<div class="collapse navbar-collapse">
+	  <ul class="nav navbar-nav">
+
+		<li class="dropdown"> 
+			<a data-toggle="dropdown" href="#">Ordonnances<b class="caret"></b></a>
+			<ul class="dropdown-menu">
+				<li><a href="#">Créer une ordonnance</a></li>
+				<li><a href="#">Modifier une ordonnance</a></li>
+				<li><a href="#">Imprimer une ordonnance</a></li>
+				<li><a href="#">Historique des ordonnances</a></li>
+				<li><a href="#">Statistiques</a></li>
+			</ul>
+		</li>
+
+		<li class="dropdown"> 
+			<a data-toggle="dropdown" href="#">Factures<b class="caret"></b></a>
+			<ul class="dropdown-menu">
+				<li><a href="#">Créer une facture</a></li>
+				<li><a href="#">Modifier une facture</a></li>
+				<li><a href="#">Imprimer une facture</a></li>
+				<li><a href="#">Historique des factures</a></li>
+				<li><a href="#">Statistiques</a></li>
+			</ul>
+		</li>
+
+		<li class="dropdown"> 
+			<a data-toggle="dropdown" href="#">Rendez-vous<b class="caret"></b></a>
+			<ul class="dropdown-menu">
+				<li><a href="#">Créer un rendez-vous</a></li>
+				<li><a href="#">Gérer un rendez-vous</a></li>
+				<li><a href="#">Historique des rendez-vous</a></li>
+			</ul>
+		</li>
+
+		<li class="dropdown"> 
+			<a data-toggle="dropdown" href="#">Administration<b class="caret"></b></a>
+			<ul class="dropdown-menu">
+				<li><a href="#">Créer un employé</a></li>
+				<li><a href="#">Modifier ses droits</a></li>
+			</ul>
+		</li>
+	  </ul>
+	  <form class="navbar-form navbar-right" action="deco.php">
+		<button class="btn btn-danger">Déconnexion</button>
+	  </form>
+	</div><!--/.nav-collapse -->
+  </div>
+</div>
